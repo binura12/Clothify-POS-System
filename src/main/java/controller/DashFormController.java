bc05ddbbe86158abbead1f1301c21e834749a4ad
Binura Namulitha <binura.bin02@gmail.com>
2024-10-18 22:42:35 +0530

    first commit

---
+++ src/main/java/controller/DashFormController.java
@@ -0,0 +1,44 @@
+package controller;
+
+import com.jfoenix.controls.JFXButton;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class DashFormController {
+
+    @FXML
+    private JFXButton btnAdmin;
+
+    @FXML
+    private JFXButton btnEmployee;
+
+    @FXML
+    private JFXButton btnUser;
+
+    @FXML
+    void btnAdminOnAction(ActionEvent event) {
+        Stage stage = new Stage();
+        try {
+            stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("../view/admin-login_form.fxml"))));
+            stage.show();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @FXML
+    void btnEmployeeOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnUserOnAction(ActionEvent event) {
+
+    }
+
+}
